20eda071eefd1f24452453708b1315c63dfcd774
James Moring <james.moring@kampilangroup.com>
2024-04-15 17:52:04 -0400

    Moved in DateTime EF convertors

---
+++ Fabrica.Persistence.Ef/Persistence/Ef/Converters/MaxDateTimeConvertor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Fabrica.Persistence.Ef.Converters;
+
+public class MaxDateTimeConvertor() : ValueConverter<DateTime, DateTime>( time => time != DateTime.MaxValue ? time : MaxDateTime, time => time != MaxDateTime ? time : DateTime.MaxValue )
+{
+
+    public static readonly DateTime MaxDateTime = new(2200, 1, 1, 0, 0, 0, 0);
+
+}
---
+++ Fabrica.Persistence.Ef/Persistence/Ef/Converters/MinDateTimeConvertor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Fabrica.Persistence.Ef.Converters;
+
+public class MinDateTimeConvertor() : ValueConverter<DateTime, DateTime>(time => time != DateTime.MinValue ? time : MinDateTime, time => time != MinDateTime ? time : DateTime.MinValue)
+{
+
+    public static readonly DateTime MinDateTime = new(1883, 11, 19, 0, 0, 0, 0);
+
+}
