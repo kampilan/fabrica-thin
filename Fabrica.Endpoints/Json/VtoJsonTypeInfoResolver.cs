d0b1503677cb4e851229a478d0053de0a5de8611
James Moring <james.moring@kampilangroup.com>
2024-04-17 11:30:29 -0400

    Added VtoTypeResolver

---
+++ Fabrica.Endpoints/Json/VtoJsonTypeInfoResolver.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization.Metadata;
+using System.Text.Json;
+using Fabrica.Persistence;
+
+namespace Fabrica.Json;
+
+public class VtoJsonTypeInfoResolver: CompactJsonTypeInfoResolver
+{
+
+    public override JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
+    {
+
+
+        var typeInfo = base.GetTypeInfo(type, options);
+
+        if (typeInfo.Kind != JsonTypeInfoKind.Object)
+            return typeInfo;
+
+        if( !type.IsAssignableTo(typeof(IEntity)) )
+            return typeInfo;
+
+
+        foreach (var prop in typeInfo.Properties)
+        {
+            if( prop.Name == "Id" || prop.Name.EndsWith("Id") )
+
+                prop.ShouldSerialize = (_, _) => false;
+        }
+
+        return typeInfo;
+
+
+    }
+
+}
