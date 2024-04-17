a04ac94174fccb6dbfaf7b2575d7a8882b1432f2
James Moring <james.moring@kampilangroup.com>
2024-04-17 07:18:27 -0400

    Reafctor od ValidationBuilder

--- Fabrica.Rules/ValidationBuilder.cs
+++ Fabrica.Rules/ValidationBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq.Expressions;
 using Fabrica.Rules.Builder;
 using Fabrica.Rules.Validators;
+using Fabrica.Utilities.Types;
 
 // ReSharper disable UnusedMember.Global
 
@@ -10,14 +11,15 @@ public abstract class ValidationBuilder<TFact>: AbstractRuleBuilder, IBuilder
 {
 
     private int _currentSalience = 100000;
+    private string _currentMutex = "";
 
-    public virtual IValidator<TFact,TType> For<TType>(Expression<Func<TFact,TType>> extractor)
+    public virtual IValidator<TFact,TType> Rule<TType>(Expression<Func<TFact,TType>> extractor)
     {
 
         var nameSpace = GetType().Namespace;
         var fullSetName = $"{nameSpace}.{SetName}";
 
-        var rule = new ValidationRule<TFact>(fullSetName, "Placeholder");
+        var rule = new ValidationRule<TFact>(fullSetName, typeof(TFact).GetConciseName());
 
         // Apply default salience
         rule.WithSalience(_currentSalience);
@@ -25,6 +27,9 @@ public abstract class ValidationBuilder<TFact>: AbstractRuleBuilder, IBuilder
         // Apply default inception and expiration
         rule.WithInception(DefaultInception);
         rule.WithExpiration(DefaultExpiration);
+
+        if( !string.IsNullOrWhiteSpace(_currentMutex) )
+            rule.InMutex(_currentMutex);
 
         Sinks.Add(t => t.Add(typeof(TFact), rule));
 
@@ -37,13 +42,13 @@ public abstract class ValidationBuilder<TFact>: AbstractRuleBuilder, IBuilder
     }
 
 
-    protected ValidationRule<TFact> Add()
+    protected ValidationRule<TFact> Rule()
     {
 
         var nameSpace = GetType().Namespace;
         var fullSetName = $"{nameSpace}.{SetName}";
 
-        var rule = new ValidationRule<TFact>(fullSetName, "Placeholder");
+        var rule = new ValidationRule<TFact>(fullSetName, typeof(TFact).GetConciseName());
 
         rule.WithSalience(_currentSalience);
 
@@ -51,11 +56,47 @@ public abstract class ValidationBuilder<TFact>: AbstractRuleBuilder, IBuilder
         rule.WithInception(DefaultInception);
         rule.WithExpiration(DefaultExpiration);
 
+        if (!string.IsNullOrWhiteSpace(_currentMutex))
+            rule.InMutex(_currentMutex);
+
+
         Sinks.Add(t => t.Add(typeof(TFact), rule));
 
         _currentSalience += 100;
 
         return rule;
+
+    }
+
+
+    protected void Mutex(Action builder)
+    {
+
+        try
+        {
+            _currentMutex = Ulid.NewUlid();
+            builder();
+        }
+        finally
+        {
+            _currentMutex = "";
+        }
+
+    }
+
+
+    protected void Mutex( string name, Action builder )
+    {
+
+        try
+        {
+            _currentMutex = name;
+            builder();
+        }
+        finally
+        {
+            _currentMutex = "";
+        }
 
     }
 
