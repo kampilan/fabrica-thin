01784e2e3e2c87ccbf143055ea4bf16354f74f09
James Moring <james.moring@kampilangroup.com>
2024-04-16 21:35:50 -0400

    Moved in CompactJsonTypeInfoResolver

---
+++ Fabrica.Endpoints/Json/JsonConfigAutofacExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.Extensions.DependencyInjection;
+// ReSharper disable UnusedMember.Global
+
+namespace Fabrica.Json;
+
+public static class JsonConfigAutofacExtensions
+{
+
+    public static ContainerBuilder ConfigureJsonSerializerOptions(this ContainerBuilder builder, Action<JsonSerializerOptions> optBuilder )
+    {
+
+        var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        optBuilder(options);
+
+
+        var services = new ServiceCollection();
+        services.Configure<JsonOptions>(o =>
+        {
+            o.SerializerOptions.PropertyNamingPolicy        = options.PropertyNamingPolicy;
+            o.SerializerOptions.DefaultIgnoreCondition      = options.DefaultIgnoreCondition;
+            o.SerializerOptions.DictionaryKeyPolicy         = options.DictionaryKeyPolicy;
+            o.SerializerOptions.ReferenceHandler            = options.ReferenceHandler;
+            o.SerializerOptions.PropertyNameCaseInsensitive = options.PropertyNameCaseInsensitive;
+            o.SerializerOptions.UnmappedMemberHandling      = options.UnmappedMemberHandling;
+            o.SerializerOptions.WriteIndented               = options.WriteIndented;
+            o.SerializerOptions.TypeInfoResolver            = options.TypeInfoResolver;
+        });
+
+        builder.Populate(services);
+
+        builder.RegisterInstance(options)
+            .AsSelf()
+            .SingleInstance();
+
+
+        return builder;
+    }
+
+}
---
+++ Fabrica.Endpoints/Json/PascalJsonNamingPolicy.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+// ReSharper disable UnusedMember.Global
+
+namespace Fabrica.Json;
+
+public class PascalJsonNamingPolicy : JsonNamingPolicy
+{
+
+    public override string ConvertName( string name )
+    {
+        return name;
+    }
+
+}
