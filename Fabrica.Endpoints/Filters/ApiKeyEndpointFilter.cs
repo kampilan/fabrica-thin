eea5000d7c00337bb2834ac7e5604ecf0ca94cd2
James Moring <james.moring@kampilangroup.com>
2024-04-20 10:46:08 -0400

    Added FixedTime APiKey Validator

---
+++ Fabrica.Endpoints/Filters/ApiKeyEndpointFilter.cs
@@ -0,0 +1,90 @@
+﻿
+// ReSharper disable UnusedMember.Global
+
+using Fabrica.Utilities.Container;
+using Fabrica.Watch;
+using Microsoft.AspNetCore.Http;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Fabrica.Filters;
+
+public interface IApiKeyValidator
+{
+
+    bool IsValid(string candidate);
+
+}
+
+
+public abstract class AbstractApiKeyValidator(ICorrelation correlation) : CorrelatedObject(correlation), IApiKeyValidator
+{
+
+    protected abstract string GetApiKey();
+
+    public bool IsValid(string candidate)
+    {
+
+        var apiKey = GetApiKey();
+
+        var bufA = Encoding.ASCII.GetBytes(apiKey);
+        var spanA = new ReadOnlySpan<byte>(bufA);
+
+        var bufB = Encoding.ASCII.GetBytes(candidate);
+        var spanB = new ReadOnlySpan<byte>(bufB);
+
+        var matched = CryptographicOperations.FixedTimeEquals(spanA, spanB);
+
+
+        return matched;
+
+
+    }
+
+
+}
+
+
+public class SimpleApiKeyValidator( ICorrelation correlation, string apiKey ) : AbstractApiKeyValidator(correlation)
+{
+
+    protected override string GetApiKey()
+    {
+        return apiKey;
+    }
+
+}
+
+
+
+public class ApiKeyEndpointFilter( IApiKeyValidator validator, ICorrelation correlation ) : CorrelatedObject(correlation), IEndpointFilter
+{
+
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+
+        using var logger = EnterMethod();
+
+        var header = context.HttpContext.Request.Headers["x-api-key"];
+        var key = header.FirstOrDefault();
+
+        if( string.IsNullOrWhiteSpace(key) )
+        {
+            logger.Warning("API key not present");
+            return Results.Unauthorized();
+        }
+        
+        if( !validator.IsValid(key) )
+        {
+            logger.Warning("API key not Valid");
+            return Results.Unauthorized();
+        }
+
+        return await next(context);
+
+
+    }
+
+
+}
