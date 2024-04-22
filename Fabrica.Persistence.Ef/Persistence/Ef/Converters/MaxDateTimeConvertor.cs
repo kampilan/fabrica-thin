01600f59209c4d15184cf67f2ad7c98ff8c5d0b1
James Moring <james.moring@kampilangroup.com>
2024-04-22 12:32:02 -0400

    Tweeking DateTime Converters

--- Fabrica.Persistence.Ef/Persistence/Ef/Converters/MaxDateTimeConvertor.cs
+++ Fabrica.Persistence.Ef/Persistence/Ef/Converters/MaxDateTimeConvertor.cs
@@ -5,6 +5,6 @@ namespace Fabrica.Persistence.Ef.Converters;
 public class MaxDateTimeConvertor() : ValueConverter<DateTime, DateTime>( time => time != DateTime.MaxValue ? time : MaxDateTime, time => time != MaxDateTime ? time : DateTime.MaxValue )
 {
 
-    public static readonly DateTime MaxDateTime = new(2200, 1, 1, 0, 0, 0, 0);
+    public static readonly DateTime MaxDateTime = new (2200, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
 }
--- Fabrica.Persistence.Ef/Persistence/Ef/Converters/MinDateTimeConvertor.cs
+++ Fabrica.Persistence.Ef/Persistence/Ef/Converters/MinDateTimeConvertor.cs
@@ -2,9 +2,11 @@
 
 namespace Fabrica.Persistence.Ef.Converters;
 
-public class MinDateTimeConvertor() : ValueConverter<DateTime, DateTime>(time => time != DateTime.MinValue ? time : MinDateTime, time => time != MinDateTime ? time : DateTime.MinValue)
+public class MinDateTimeConvertor() : ValueConverter<DateTime, DateTime>(time => time != DateTime.MinValue ? time : MinDateTime, time => time != MinDateTime ? DateTime.SpecifyKind(time,DateTimeKind.Utc) : DateTime.SpecifyKind(DateTime.MinValue,DateTimeKind.Utc)  )
 {
 
-    public static readonly DateTime MinDateTime = new(1883, 11, 19, 0, 0, 0, 0);
+    public static readonly DateTime MinDateTime = new ( 1883, 11, 19, 0, 0, 0, 0, DateTimeKind.Utc );
+
+
 
 }
