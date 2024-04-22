909aacaa0a75d1e9d239f0ed9f017c738de4afcf
James Moring <james.moring@kampilangroup.com>
2024-04-22 09:19:32 -0400

    Added Is08601 DateTime Converter

---
+++ Fabrica.Core/Utilities/Text/Json/Iso8601DateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Fabrica.Utilities.Text.Json;
+
+public class Iso8601DateTimeConverter : JsonConverter<DateTime>
+{
+    
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.GetDateTime().ToUniversalTime();
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        var dt = value.ToUniversalTime();
+        var iso = dt.ToString("O");
+        writer.WriteStringValue(iso);
+    }
+
+}
