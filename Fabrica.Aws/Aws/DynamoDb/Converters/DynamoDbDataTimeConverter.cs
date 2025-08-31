d0ac2b9dfeeec14e77a53fa16ac566bd58ba661f
jmoring <james.moring@pondhawktech.com>
2025-08-30 20:27:10 -0400

    Added `DynamoDbDataTimeConverter` to handle local datetime serialization and deserialization for DynamoDB.

---
+++ Fabrica.Aws/Aws/DynamoDb/Converters/DynamoDbDataTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace Fabrica.Aws.DynamoDb.Converters;
+
+public class DynamoDbDataTimeConverter: IPropertyConverter
+{
+
+    public object FromEntry(DynamoDBEntry entry)
+    {
+
+        var str = entry.AsString();
+        var utc = DateTime.Parse(str);
+        var local = DateTime.SpecifyKind(utc, DateTimeKind.Local);
+        
+        return local;
+        
+    } 
+
+    public DynamoDBEntry ToEntry(object value)
+    {
+        
+        var dt = ((DateTime)value).ToUniversalTime();
+        var str = dt.ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+
+        return str;
+        
+    }    
+    
+    
+}
